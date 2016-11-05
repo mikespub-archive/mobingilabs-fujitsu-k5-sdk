b490202f43d6e6906dba64928c2ef24275083acb
wayland <waylandzhang@gmail.com>
2016-11-05 13:06:06 +0900

    add network create, keypair create

--- src/K5/Networking/Network.php
+++ src/K5/Networking/Network.php
@@ -70,4 +70,39 @@ class Network extends Auth
 
     }
 
+
+
+
+    /**
+     * Create a Network against K5 API
+     *
+     * @see https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/networks
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     *
+     * @region specific
+     *
+     * @\K5\Networking\getNetworks()
+     *
+     * @return string
+     */
+    public function createNetwork($region, $data){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X POST https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/networks \
+    	-H "Content-Type: application/json" \
+    	-H "X-Auth-Token: '. $Auth['token'] .'" \
+        -d \''. $data .'\' \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
+
 }
