efe54dc5db15a84baf79d93238d2fb34960e8bb7
waylandzhang <waylandzhang@gmail.com>
2016-11-07 23:16:02 +0900

    firewall rule and policy creation

--- src/K5/Networking/Firewall.php
+++ src/K5/Networking/Firewall.php
@@ -37,6 +37,37 @@ class Firewall extends Auth
 
     }
 
+        /**
+         * Create Firewalls against K5 API
+         *
+         * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewalls
+         *
+         * @param $token                Token used for HTTP request header authentication
+         * @param $region               Specify region
+         *
+         * @region specific
+         *
+         * @\K5\Networking\Firewall\getFirewalls()
+         *
+         * @return string
+         */
+        public function createFirewalls($region,$data){
+
+            $Auth = Auth::getAuthToken();
+
+            $c = '\
+            curl -X POST https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewalls \
+        	-H "Content-Type: application/json" \
+        	-H "X-Auth-Token: '. $Auth['token'] .'" \
+            -d \''. $data .'\' \
+            ';
+
+            $respond = exec($c);
+
+            return $respond;
+
+        }
+
 
     /**
      * List All Firewall Policies against K5 API
@@ -70,6 +101,38 @@ class Firewall extends Auth
 
 
     /**
+     * Create Firewall Policies against K5 API
+     *
+     * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewall_policies
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     *
+     * @region specific
+     *
+     * @\K5\Networking\Firewall\getFirewallPolicies()
+     *
+     * @return string
+     */
+    public function createFirewallPolicies($region,$data){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X POST https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewall_policies \
+    	-H "Content-Type: application/json" \
+    	-H "X-Auth-Token: '. $Auth['token'] .'" \
+        -d \''. $data .'\' \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
+
+    /**
      * List All Firewall Rules against K5 API
      *
      * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewall_rules
@@ -91,6 +154,38 @@ class Firewall extends Auth
         curl -X GET https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewall_rules \
     	-H "Content-Type: application/json" \
     	-H "X-Auth-Token: '. $Auth['token'] .'" \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
+
+    /**
+     * Create Firewall Rules against K5 API
+     *
+     * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewall_rules
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     *
+     * @region specific
+     *
+     * @\K5\Networking\Firewall\getFirewallRules()
+     *
+     * @return string
+     */
+    public function createFirewallRules($region,$data){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X POST https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewall_rules \
+    	-H "Content-Type: application/json" \
+    	-H "X-Auth-Token: '. $Auth['token'] .'" \
+        -d \''. $data .'\' \
         ';
 
         $respond = exec($c);
