dcd31fe1f3a22e3cc4e7c66c76e43f83cf411180
wayland <waylandzhang@gmail.com>
2016-11-08 11:42:09 +0900

    update vm

--- src/K5/Compute/VirtualServer.php
+++ src/K5/Compute/VirtualServer.php
@@ -103,6 +103,42 @@ class VirtualServer extends Auth
 
 
 
+
+
+
+    /**
+     * Update a VM
+     *
+     * @see https://compute.jp-east-1.cloud.global.fujitsu.com/v2/{PROJECT_ID}/servers/{SERVER_ID}
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     * @param $server_id            The server id to query
+     *
+     * @region specific
+     *
+     * @\K5\Networking\getServerInfo()
+     *
+     * @return string
+     */
+    public function updateServer($region, $server_id, $data){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X PUT https://compute.' .$region. '.cloud.global.fujitsu.com/v2/' .$Auth['project_id']. '/servers/' .$server_id. ' \
+    	-H "Content-Type: application/json" \
+    	-H "X-Auth-Token: '. $Auth['token'] .'" \
+        -d \''. $data .'\' \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
+
     /**
      * Retrieve server port ID, which is required for assigning global IP addresses to the virtual server
      *
