8dbaecaf88b306223d503d5eb9caa80a332973f7
wayland <waylandzhang@gmail.com>
2016-11-08 15:21:58 +0900

    reset connection firewall on router

--- src/K5/Networking/Firewall.php
+++ src/K5/Networking/Firewall.php
@@ -70,6 +70,37 @@ class Firewall extends Auth
 
 
     /**
+     * Delete Firewalls against K5 API
+     *
+     * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewalls
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     *
+     * @region specific
+     *
+     * @\K5\Networking\Firewall\getFirewalls()
+     *
+     * @return string
+     */
+    public function deleteFirewalls($region,$firewall_id){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X DELETE https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewalls/'.$firewall_id.' \
+        -H "Content-Type: application/json" \
+        -d \''. $data .'\' \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
+
+    /**
      * Update Firewalls against K5 API
      *
      * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewalls
@@ -89,6 +120,38 @@ class Firewall extends Auth
 
         $c = '\
         curl -X PUT https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewalls/'.$firewall_id.' \
+    	-H "Content-Type: application/json" \
+    	-H "X-Auth-Token: '. $Auth['token'] .'" \
+        -d \''. $data .'\' \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
+
+    /**
+     * Connection reset for applying firewall rule to the current communication immediately.
+     * All connections managed by routers that use the specified firewall will be deleted a few seconds after the API response. This reflects the rules set for the firewall to communication. Existing communications that were in progress via routers that use the specified firewall, including communications permitted by it, will be disconnected at the time. If any communication permission rules exist, the disconnection will be temporary, so communication will be resumed.
+     * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewalls
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     *
+     * @region specific
+     *
+     * @\K5\Networking\Firewall\getFirewalls()
+     *
+     * @return string
+     */
+    public function resetConnectionFirewalls($region,$firewall_id,$data){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X PUT https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewalls/'.$firewall_id.'/reset_connections \
     	-H "Content-Type: application/json" \
     	-H "X-Auth-Token: '. $Auth['token'] .'" \
         -d \''. $data .'\' \
