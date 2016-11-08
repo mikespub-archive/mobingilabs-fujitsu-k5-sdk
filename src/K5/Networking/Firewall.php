9406c08efe19723806ae6f705e954fc709cf6e28
wayland <waylandzhang@gmail.com>
2016-11-08 13:42:30 +0900

    update firewall rule

--- src/K5/Networking/Firewall.php
+++ src/K5/Networking/Firewall.php
@@ -262,5 +262,38 @@ class Firewall extends Auth
 
 
 
+    /**
+     * Update Firewall Rules against K5 API
+     *
+     * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewall_rules
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     *
+     * @region specific
+     *
+     * @\K5\Networking\Firewall\getFirewallRules()
+     *
+     * @return string
+     */
+    public function updateFirewallRules($region,$fwrule_id,$data){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X POST https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewall_rules/'.$fwrule_id.' \
+        -H "Content-Type: application/json" \
+        -H "X-Auth-Token: '. $Auth['token'] .'" \
+        -d \''. $data .'\' \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
+
+
 
 }
