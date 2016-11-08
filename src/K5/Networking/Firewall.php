bb9e4f945ee6d49dd6258437ac2df7224dd49401
wayland <waylandzhang@gmail.com>
2016-11-08 13:11:47 +0900

    updateFirewall

--- src/K5/Networking/Firewall.php
+++ src/K5/Networking/Firewall.php
@@ -37,36 +37,69 @@ class Firewall extends Auth
 
     }
 
-        /**
-         * Create Firewalls against K5 API
-         *
-         * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewalls
-         *
-         * @param $token                Token used for HTTP request header authentication
-         * @param $region               Specify region
-         *
-         * @region specific
-         *
-         * @\K5\Networking\Firewall\getFirewalls()
-         *
-         * @return string
-         */
-        public function createFirewalls($region,$data){
+    /**
+     * Create Firewalls against K5 API
+     *
+     * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewalls
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     *
+     * @region specific
+     *
+     * @\K5\Networking\Firewall\getFirewalls()
+     *
+     * @return string
+     */
+    public function createFirewalls($region,$data){
 
-            $Auth = Auth::getAuthToken();
+        $Auth = Auth::getAuthToken();
 
-            $c = '\
-            curl -X POST https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewalls \
-        	-H "Content-Type: application/json" \
-        	-H "X-Auth-Token: '. $Auth['token'] .'" \
-            -d \''. $data .'\' \
-            ';
+        $c = '\
+        curl -X POST https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewalls \
+    	-H "Content-Type: application/json" \
+    	-H "X-Auth-Token: '. $Auth['token'] .'" \
+        -d \''. $data .'\' \
+        ';
 
-            $respond = exec($c);
+        $respond = exec($c);
 
-            return $respond;
+        return $respond;
 
-        }
+    }
+
+
+    /**
+     * Update Firewalls against K5 API
+     *
+     * https://networking.jp-east-1.cloud.global.fujitsu.com/v2.0/fw/firewalls
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     *
+     * @region specific
+     *
+     * @\K5\Networking\Firewall\getFirewalls()
+     *
+     * @return string
+     */
+    public function updateFirewalls($region,$firewall_id,$data){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X POST https://networking.' .$region. '.cloud.global.fujitsu.com/v2.0/fw/firewalls/'.$firewall_id.' \
+    	-H "Content-Type: application/json" \
+    	-H "X-Auth-Token: '. $Auth['token'] .'" \
+        -d \''. $data .'\' \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
 
 
     /**
