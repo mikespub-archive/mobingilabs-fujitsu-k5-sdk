508c56bfaea624a53a3dd6ee1feca8a97e73d6e2
SnhrK <snhrK@snhrK-no-MacBook-Pro.local>
2016-11-21 16:05:41 +0900

    test loadbalancer

--- src/K5/Networking/LoadBalancer.php
+++ src/K5/Networking/LoadBalancer.php
@@ -60,8 +60,8 @@ class LoadBalancer extends Auth
         $Auth = Auth::getAuthToken();
 
         $c = '\
-        curl -X GET https://loadbalancing.' .$region. '.cloud.global.fujitsu.com/?Action=CreateLoadBalancer \
-        &LoadBalancerName='.$loadblancername.' \
+        curl -X POST https://loadbalancing.' .$region. '.cloud.global.fujitsu.com/?Action=CreateLoadBalancer
+        &LoadBalancerName='.$loadblancername.'
         &Subnets.member='.$subnet.' \
     	-H "Content-Type: application/json" \
     	-H "X-Auth-Token: '. $Auth['token'] .'" \
@@ -72,6 +72,49 @@ class LoadBalancer extends Auth
         return $respond;
 
     }
+
+    /**
+     * create LoadBalancerListeners against K5 API
+     *
+     *https://loadbalancing.ja-east-1.cloud.global.fujitsu.com/?Action=CreateLoadBalancerListeners&LoadBalancerName=MyLB01&Protocol=https&LoadBalancerPort=443&InstancePort=80&InstanceProtocol=http&SSLCertificateId=1232d7bf-8f28-4cc7-a63d-44e218853c6d
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     * @param $loadbalanername      LoadBalancerName require
+     * @param $Protocol             http require
+     * @param $LoadBalancerPort     80 require
+     * @param $InstancePort         80 require
+     * @param $InstanceProtocol     http  require
+     * @param $SSLCertificateId     SSLCertificateId require
+     *
+     * @region specific
+     *
+     * @\K5\Networking\LoadBalancer\createLoadBalancerLsitner()
+     *
+     * @return string
+     */
+    public function createLoadBalancerListener($region, $loadblancername, $subnet){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X POST https://loadbalancing.' .$region. '.cloud.global.fujitsu.com/?Action=CreateLoadBalancerListeners
+        &LoadBalancerName='.$loadblancername.'
+        &Protocol='.$protocol.'
+        &LoadBalancerPort='.$loadbalancerport.'
+        &InstancePort='.$instaneport.'
+        &InstanceProtocol='.$instanceprotocol.'
+        &SSLCertificateId='.$sslcertificateid.' \
+      -H "Content-Type: application/json" \
+      -H "X-Auth-Token: '. $Auth['token'] .'" \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
+
 
 
 }
