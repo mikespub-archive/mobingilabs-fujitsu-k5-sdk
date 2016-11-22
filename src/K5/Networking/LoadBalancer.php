a8c269c4926db6b3b112a59b79cdcbd2397ec094
SnhrK <snhrK@snhrK-no-MacBook-Pro.local>
2016-11-22 16:40:50 +0900

    add deleteloadbalancer

--- src/K5/Networking/LoadBalancer.php
+++ src/K5/Networking/LoadBalancer.php
@@ -116,5 +116,36 @@ class LoadBalancer extends Auth
     }
 
 
+    /**
+     * delete LoadBalancer against K5 API
+     *
+     *https://loadbalancing.ja-east-1.cloud.global.fujitsu.com/?Action=DeleteLoadBalancer&LoadBalancerName=MyLB01
+     *
+     * @param $token                Token used for HTTP request header authentication
+     * @param $region               Specify region
+     * @param $loadbalanername      LoadBalancerName require
+     *
+     * @region specific
+     *
+     * @\K5\Networking\LoadBalancer\deleteLoadBalancer()
+     *
+     * @return string
+     */
+    public function deleteLoadBalancer($region, $loadblancername){
+
+        $Auth = Auth::getAuthToken();
+
+        $c = '\
+        curl -X DELETE https://loadbalancing.' .$region. '.cloud.global.fujitsu.com/?Action=DeleteLoadBalancer
+        &LoadBalancerName='.$loadblancername.' \
+      -H "Content-Type: application/json" \
+      -H "X-Auth-Token: '. $Auth['token'] .'" \
+        ';
+
+        $respond = exec($c);
+
+        return $respond;
+
+    }
 
 }
